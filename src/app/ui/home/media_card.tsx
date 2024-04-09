e1e601d59c3499654c83a86fd759f3e63f8c09d8
Jiehao Luo <powerseed5044@gmail.com>
2024-04-08 22:31:30 -0600

    Buttons for post in Home page

--- src/app/ui/home/media_card.tsx
+++ src/app/ui/home/media_card.tsx
@@ -3,6 +3,7 @@ import en from 'javascript-time-ago/locale/en';
 import ReactTimeAgo from 'react-time-ago';
 import './style.css'
 import Slider from 'react-slick';
+import { MouseEvent } from 'react';
 
 type MediaCardProps = {
     avatar: string,
@@ -13,8 +14,14 @@ type MediaCardProps = {
     images: string[]
 }
 
+enum OperationsOnButtonsForPost {
+    HOVER,
+    LEAVE
+}
+
 export default function MediaCard(props: MediaCardProps) {
-    TimeAgo.addDefaultLocale(en);
+    TimeAgo.setDefaultLocale(en.locale);
+    TimeAgo.addLocale(en);
 
     const SliderNavigationButton = (
         props: {
@@ -41,6 +48,29 @@ export default function MediaCard(props: MediaCardProps) {
         className: "bg-black rounded"
     };
 
+    function onButtonsForPostHoverOrLeave(event: MouseEvent, operation: OperationsOnButtonsForPost) {
+        let img = event.target! as HTMLImageElement;
+        let newSrc;
+
+        switch (operation) {
+            case OperationsOnButtonsForPost.HOVER:
+                newSrc = `/home/${img.id}-hover.svg`;
+                break;
+            case OperationsOnButtonsForPost.LEAVE:
+                newSrc = `/home/${img.id}.svg`;
+                break;
+        }
+
+        img.src = newSrc;
+
+        if (operation === OperationsOnButtonsForPost.LEAVE && img.id === 'like') {
+            img.style.scale = '1.2';
+            setTimeout(() => {
+                img.style.scale = '1';
+            }, 100)
+        }
+    }
+
     return (
         <div className="flex flex-col space-y-3 w-[450px] h-[850px]">
             <div className="flex justify-between w-full h-min">
@@ -58,7 +88,7 @@ export default function MediaCard(props: MediaCardProps) {
                             {
                                 props.isVerified &&
                                 <div className="w-[15px] h-[15px]">
-                                    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" id="verified" className="icon glyph" fill="#0000ff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M21.6,9.84A4.57,4.57,0,0,1,21.18,9,4,4,0,0,1,21,8.07a4.21,4.21,0,0,0-.64-2.16,4.25,4.25,0,0,0-1.87-1.28,4.77,4.77,0,0,1-.85-.43A5.11,5.11,0,0,1,17,3.54a4.2,4.2,0,0,0-1.8-1.4A4.22,4.22,0,0,0,13,2.21a4.24,4.24,0,0,1-1.94,0,4.22,4.22,0,0,0-2.24-.07A4.2,4.2,0,0,0,7,3.54a5.11,5.11,0,0,1-.66.66,4.77,4.77,0,0,1-.85.43A4.25,4.25,0,0,0,3.61,5.91,4.21,4.21,0,0,0,3,8.07,4,4,0,0,1,2.82,9a4.57,4.57,0,0,1-.42.82A4.3,4.3,0,0,0,1.63,12a4.3,4.3,0,0,0,.77,2.16,4,4,0,0,1,.42.82,4.11,4.11,0,0,1,.15.95,4.19,4.19,0,0,0,.64,2.16,4.25,4.25,0,0,0,1.87,1.28,4.77,4.77,0,0,1,.85.43,5.11,5.11,0,0,1,.66.66,4.12,4.12,0,0,0,1.8,1.4,3,3,0,0,0,.87.13A6.66,6.66,0,0,0,11,21.81a4,4,0,0,1,1.94,0,4.33,4.33,0,0,0,2.24.06,4.12,4.12,0,0,0,1.8-1.4,5.11,5.11,0,0,1,.66-.66,4.77,4.77,0,0,1,.85-.43,4.25,4.25,0,0,0,1.87-1.28A4.19,4.19,0,0,0,21,15.94a4.11,4.11,0,0,1,.15-.95,4.57,4.57,0,0,1,.42-.82A4.3,4.3,0,0,0,22.37,12,4.3,4.3,0,0,0,21.6,9.84Zm-4.89.87-5,5a1,1,0,0,1-1.42,0l-3-3a1,1,0,1,1,1.42-1.42L11,13.59l4.29-4.3a1,1,0,0,1,1.42,1.42Z" style={{ fill: '#19BDFF' }}></path></g></svg>
+                                    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" id="verified" className="icon glyph" fill="#0000ff"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M21.6,9.84A4.57,4.57,0,0,1,21.18,9,4,4,0,0,1,21,8.07a4.21,4.21,0,0,0-.64-2.16,4.25,4.25,0,0,0-1.87-1.28,4.77,4.77,0,0,1-.85-.43A5.11,5.11,0,0,1,17,3.54a4.2,4.2,0,0,0-1.8-1.4A4.22,4.22,0,0,0,13,2.21a4.24,4.24,0,0,1-1.94,0,4.22,4.22,0,0,0-2.24-.07A4.2,4.2,0,0,0,7,3.54a5.11,5.11,0,0,1-.66.66,4.77,4.77,0,0,1-.85.43A4.25,4.25,0,0,0,3.61,5.91,4.21,4.21,0,0,0,3,8.07,4,4,0,0,1,2.82,9a4.57,4.57,0,0,1-.42.82A4.3,4.3,0,0,0,1.63,12a4.3,4.3,0,0,0,.77,2.16,4,4,0,0,1,.42.82,4.11,4.11,0,0,1,.15.95,4.19,4.19,0,0,0,.64,2.16,4.25,4.25,0,0,0,1.87,1.28,4.77,4.77,0,0,1,.85.43,5.11,5.11,0,0,1,.66.66,4.12,4.12,0,0,0,1.8,1.4,3,3,0,0,0,.87.13A6.66,6.66,0,0,0,11,21.81a4,4,0,0,1,1.94,0,4.33,4.33,0,0,0,2.24.06,4.12,4.12,0,0,0,1.8-1.4,5.11,5.11,0,0,1,.66-.66,4.77,4.77,0,0,1,.85-.43,4.25,4.25,0,0,0,1.87-1.28A4.19,4.19,0,0,0,21,15.94a4.11,4.11,0,0,1,.15-.95,4.57,4.57,0,0,1,.42-.82A4.3,4.3,0,0,0,22.37,12,4.3,4.3,0,0,0,21.6,9.84Zm-4.89.87-5,5a1,1,0,0,1-1.42,0l-3-3a1,1,0,1,1,1.42-1.42L11,13.59l4.29-4.3a1,1,0,0,1,1.42,1.42Z" style={{ fill: '#19BDFF' }}></path></g></svg>
                                 </div>
                             }
 
@@ -105,6 +135,31 @@ export default function MediaCard(props: MediaCardProps) {
                         )
                     })}
                 </Slider>
+            </div>
+
+            <div className='flex justify-between'>
+                <div className='flex space-x-4'>
+                    <img id="like" className='transition-all' src="/home/like.svg" alt="Like" width={24} height={24}
+                        onMouseOver={(event) => onButtonsForPostHoverOrLeave(event, OperationsOnButtonsForPost.HOVER)}
+                        onMouseLeave={(event) => onButtonsForPostHoverOrLeave(event, OperationsOnButtonsForPost.LEAVE)}
+                    />
+
+                    <img id="comment" src="/home/comment.svg" alt="Comment" width={24} height={24}
+                        onMouseOver={(event) => onButtonsForPostHoverOrLeave(event, OperationsOnButtonsForPost.HOVER)}
+                        onMouseLeave={(event) => onButtonsForPostHoverOrLeave(event, OperationsOnButtonsForPost.LEAVE)}
+                    />
+
+                    <img id="share-post" src="/home/share-post.svg" alt="Share Post" width={24} height={24}
+                        onMouseOver={(event) => onButtonsForPostHoverOrLeave(event, OperationsOnButtonsForPost.HOVER)}
+                        onMouseLeave={(event) => onButtonsForPostHoverOrLeave(event, OperationsOnButtonsForPost.LEAVE)} />
+                </div>
+
+                <div>
+                    <img id="save" src="/home/save.svg" alt="Save" width={24} height={24}
+                        onMouseOver={(event) => onButtonsForPostHoverOrLeave(event, OperationsOnButtonsForPost.HOVER)}
+                        onMouseLeave={(event) => onButtonsForPostHoverOrLeave(event, OperationsOnButtonsForPost.LEAVE)}
+                    />
+                </div>
             </div>
         </div>
     )
