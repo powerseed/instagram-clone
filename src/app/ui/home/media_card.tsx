d34babd360401e792a936cc0b19766fd9bf93cab
Jiehao Luo <powerseed5044@gmail.com>
2024-04-11 18:59:59 -0600

    More menu of Media Card

--- src/app/ui/home/media_card.tsx
+++ src/app/ui/home/media_card.tsx
@@ -9,6 +9,7 @@ import { MouseEvent, useRef, useState } from 'react';
 import TextareaAutosize from 'react-textarea-autosize';
 import data from '@emoji-mart/data'
 import Picker from '@emoji-mart/react'
+import MoreMenu from './more_menu';
 
 type MediaCardProps = {
     avatar: string,
@@ -32,6 +33,7 @@ export default function MediaCard(props: MediaCardProps) {
 
     let emojiPicker = useRef<HTMLDivElement>(null);
     let [comment, setComment] = useState('');
+    let [isMoreMenuOpen, setIsMoreMenuOpen] = useState(false);
 
     const SliderNavigationButton = (
         props: {
@@ -111,134 +113,143 @@ export default function MediaCard(props: MediaCardProps) {
         setComment(comment + emoji);
     }
 
-    return (
-        <div className="flex flex-col space-y-3 w-[450px] h-max-[850px]">
-            <div className="flex justify-between w-full h-min">
-                <div className="flex space-x-2">
-                    <div className="avatar-container flex items-center justify-center w-[42px] h-[42px]">
-                        <img className="avatar" src={`/home/${props.avatar}`} alt="avatar" width={38} height={38} />
-                    </div>
+    function handleCloseMoreMenu() {
+        setIsMoreMenuOpen(false);
+    }
 
-                    <div className="flex flex-col text-[12px] justify-center">
-                        <div className="flex space-x-1">
-                            <div className="font-semibold">
-                                {props.username}
+    return (
+        <>
+            <div className="flex flex-col space-y-3 w-[450px] h-max-[850px]">
+                <div className="flex justify-between w-full h-min">
+                    <div className="flex space-x-2">
+                        <div className="avatar-container flex items-center justify-center w-[42px] h-[42px]">
+                            <img className="avatar" src={`/home/${props.avatar}`} alt="avatar" width={38} height={38} />
+                        </div>
+
+                        <div className="flex flex-col text-[12px] justify-center">
+                            <div className="flex space-x-1">
+                                <div className="font-semibold">
+                                    {props.username}
+                                </div>
+
+                                {
+                                    props.isVerified &&
+                                    <div className="w-[15px] h-[15px]">
+                                        <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" id="verified" className="icon glyph" fill="#0000ff"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M21.6,9.84A4.57,4.57,0,0,1,21.18,9,4,4,0,0,1,21,8.07a4.21,4.21,0,0,0-.64-2.16,4.25,4.25,0,0,0-1.87-1.28,4.77,4.77,0,0,1-.85-.43A5.11,5.11,0,0,1,17,3.54a4.2,4.2,0,0,0-1.8-1.4A4.22,4.22,0,0,0,13,2.21a4.24,4.24,0,0,1-1.94,0,4.22,4.22,0,0,0-2.24-.07A4.2,4.2,0,0,0,7,3.54a5.11,5.11,0,0,1-.66.66,4.77,4.77,0,0,1-.85.43A4.25,4.25,0,0,0,3.61,5.91,4.21,4.21,0,0,0,3,8.07,4,4,0,0,1,2.82,9a4.57,4.57,0,0,1-.42.82A4.3,4.3,0,0,0,1.63,12a4.3,4.3,0,0,0,.77,2.16,4,4,0,0,1,.42.82,4.11,4.11,0,0,1,.15.95,4.19,4.19,0,0,0,.64,2.16,4.25,4.25,0,0,0,1.87,1.28,4.77,4.77,0,0,1,.85.43,5.11,5.11,0,0,1,.66.66,4.12,4.12,0,0,0,1.8,1.4,3,3,0,0,0,.87.13A6.66,6.66,0,0,0,11,21.81a4,4,0,0,1,1.94,0,4.33,4.33,0,0,0,2.24.06,4.12,4.12,0,0,0,1.8-1.4,5.11,5.11,0,0,1,.66-.66,4.77,4.77,0,0,1,.85-.43,4.25,4.25,0,0,0,1.87-1.28A4.19,4.19,0,0,0,21,15.94a4.11,4.11,0,0,1,.15-.95,4.57,4.57,0,0,1,.42-.82A4.3,4.3,0,0,0,22.37,12,4.3,4.3,0,0,0,21.6,9.84Zm-4.89.87-5,5a1,1,0,0,1-1.42,0l-3-3a1,1,0,1,1,1.42-1.42L11,13.59l4.29-4.3a1,1,0,0,1,1.42,1.42Z" style={{ fill: '#19BDFF' }}></path></g></svg>
+                                    </div>
+                                }
+
+                                <div className="text-gray-400 create-date">
+                                    <ReactTimeAgo date={props.created_on} timeStyle="twitter" />
+                                </div>
                             </div>
 
                             {
-                                props.isVerified &&
-                                <div className="w-[15px] h-[15px]">
-                                    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" id="verified" className="icon glyph" fill="#0000ff"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M21.6,9.84A4.57,4.57,0,0,1,21.18,9,4,4,0,0,1,21,8.07a4.21,4.21,0,0,0-.64-2.16,4.25,4.25,0,0,0-1.87-1.28,4.77,4.77,0,0,1-.85-.43A5.11,5.11,0,0,1,17,3.54a4.2,4.2,0,0,0-1.8-1.4A4.22,4.22,0,0,0,13,2.21a4.24,4.24,0,0,1-1.94,0,4.22,4.22,0,0,0-2.24-.07A4.2,4.2,0,0,0,7,3.54a5.11,5.11,0,0,1-.66.66,4.77,4.77,0,0,1-.85.43A4.25,4.25,0,0,0,3.61,5.91,4.21,4.21,0,0,0,3,8.07,4,4,0,0,1,2.82,9a4.57,4.57,0,0,1-.42.82A4.3,4.3,0,0,0,1.63,12a4.3,4.3,0,0,0,.77,2.16,4,4,0,0,1,.42.82,4.11,4.11,0,0,1,.15.95,4.19,4.19,0,0,0,.64,2.16,4.25,4.25,0,0,0,1.87,1.28,4.77,4.77,0,0,1,.85.43,5.11,5.11,0,0,1,.66.66,4.12,4.12,0,0,0,1.8,1.4,3,3,0,0,0,.87.13A6.66,6.66,0,0,0,11,21.81a4,4,0,0,1,1.94,0,4.33,4.33,0,0,0,2.24.06,4.12,4.12,0,0,0,1.8-1.4,5.11,5.11,0,0,1,.66-.66,4.77,4.77,0,0,1,.85-.43,4.25,4.25,0,0,0,1.87-1.28A4.19,4.19,0,0,0,21,15.94a4.11,4.11,0,0,1,.15-.95,4.57,4.57,0,0,1,.42-.82A4.3,4.3,0,0,0,22.37,12,4.3,4.3,0,0,0,21.6,9.84Zm-4.89.87-5,5a1,1,0,0,1-1.42,0l-3-3a1,1,0,1,1,1.42-1.42L11,13.59l4.29-4.3a1,1,0,0,1,1.42,1.42Z" style={{ fill: '#19BDFF' }}></path></g></svg>
+                                props.annotation &&
+                                <div>
+                                    {props.annotation}
                                 </div>
                             }
 
-                            <div className="text-gray-400 create-date">
-                                <ReactTimeAgo date={props.created_on} timeStyle="twitter" />
-                            </div>
                         </div>
-
-                        {
-                            props.annotation &&
-                            <div>
-                                {props.annotation}
-                            </div>
-                        }
-
                     </div>
-                </div>
 
-                <div className="flex items-center">
-                    <img src="/home/three-dot-button.svg" alt="more" width={24} height={24} />
-                </div>
-            </div>
-
-            <div>
-                <Slider {...settings}
-                    prevArrow={
-                        <SliderNavigationButton>
-                            <div className="next-slick-arrow">
-                            </div>
-                        </SliderNavigationButton>
-                    }
-                    nextArrow={
-                        <SliderNavigationButton>
-                            <div className="next-slick-arrow">
-                            </div>
-                        </SliderNavigationButton>
-                    }
-                >
-                    {props.images.map((image, index) => {
-                        return (
-                            <div key={index} className='mb-[-7px]'>
-                                <img className='rounded' src={`/home/${image}`} alt={index.toString()} />
-                            </div>
-                        )
-                    })}
-                </Slider>
-            </div>
-
-            <div className='flex justify-between'>
-                <div className='flex space-x-4'>
-                    <img id="like" className='transition-all' src="/home/like.svg" alt="Like" width={24} height={24}
-                        onMouseOver={(event) => onButtonsForPostHoverOrLeave(event, OperationsOnButtonsForPost.HOVER)}
-                        onMouseLeave={(event) => onButtonsForPostHoverOrLeave(event, OperationsOnButtonsForPost.LEAVE)}
-                    />
-
-                    <img id="comment" src="/home/comment.svg" alt="Comment" width={24} height={24}
-                        onMouseOver={(event) => onButtonsForPostHoverOrLeave(event, OperationsOnButtonsForPost.HOVER)}
-                        onMouseLeave={(event) => onButtonsForPostHoverOrLeave(event, OperationsOnButtonsForPost.LEAVE)}
-                    />
-
-                    <img id="share-post" src="/home/share-post.svg" alt="Share Post" width={24} height={24}
-                        onMouseOver={(event) => onButtonsForPostHoverOrLeave(event, OperationsOnButtonsForPost.HOVER)}
-                        onMouseLeave={(event) => onButtonsForPostHoverOrLeave(event, OperationsOnButtonsForPost.LEAVE)} />
+                    <div className="flex items-center">
+                        <img className='cursor-pointer' src="/home/three-dot-button.svg" alt="more" width={24} height={24} onClick={() => setIsMoreMenuOpen(true)} />
+                    </div>
                 </div>
 
                 <div>
-                    <img id="save" src="/home/save.svg" alt="Save" width={24} height={24}
-                        onMouseOver={(event) => onButtonsForPostHoverOrLeave(event, OperationsOnButtonsForPost.HOVER)}
-                        onMouseLeave={(event) => onButtonsForPostHoverOrLeave(event, OperationsOnButtonsForPost.LEAVE)}
-                    />
+                    <Slider {...settings}
+                        prevArrow={
+                            <SliderNavigationButton>
+                                <div className="next-slick-arrow">
+                                </div>
+                            </SliderNavigationButton>
+                        }
+                        nextArrow={
+                            <SliderNavigationButton>
+                                <div className="next-slick-arrow">
+                                </div>
+                            </SliderNavigationButton>
+                        }
+                    >
+                        {props.images.map((image, index) => {
+                            return (
+                                <div key={index} className='mb-[-7px]'>
+                                    <img className='rounded' src={`/home/${image}`} alt={index.toString()} />
+                                </div>
+                            )
+                        })}
+                    </Slider>
                 </div>
-            </div>
 
-            <div className='text-[14px]'>
-                Liked by <span className='font-medium'>{props.likedBy}</span> and <span className='font-medium'>others</span>
-            </div>
-
-            <div className='text-[13px] text-gray-500 !mt-[5px]'>
-                View all {props.commentNumber} comments
-            </div>
-
-            <div className='flex justify-between items-center !mt-[5px] text-[14px] space-x-3'>
-                <TextareaAutosize
-                    value={comment}
-                    className='w-full focus:outline-none resize-none'
-                    placeholder='Add a comment...'
-                    maxRows={4}
-                    onChange={(e) => setComment(e.target.value)}
-                />
-
-                {
-                    comment && <div className='text-sky-600 hover:text-black cursor-pointer font-medium'>
-                        Post
-                    </div>
-                }
-
-                <div className='flex relative'>
-                    <button className='self-center' onClick={(event) => onEmojiButtonClick(event)}>
-                        <img id="emoji" src="/home/emoji.svg" alt="Emoji" width={24} height={24}
+                <div className='flex justify-between'>
+                    <div className='flex space-x-4'>
+                        <img id="like" className='transition-all' src="/home/like.svg" alt="Like" width={24} height={24}
                             onMouseOver={(event) => onButtonsForPostHoverOrLeave(event, OperationsOnButtonsForPost.HOVER)}
                             onMouseLeave={(event) => onButtonsForPostHoverOrLeave(event, OperationsOnButtonsForPost.LEAVE)}
                         />
-                    </button>
 
-                    <div ref={emojiPicker} className='hidden absolute z-10'>
-                        <Picker data={data} onEmojiSelect={onEmojiSelect} />
+                        <img id="comment" src="/home/comment.svg" alt="Comment" width={24} height={24}
+                            onMouseOver={(event) => onButtonsForPostHoverOrLeave(event, OperationsOnButtonsForPost.HOVER)}
+                            onMouseLeave={(event) => onButtonsForPostHoverOrLeave(event, OperationsOnButtonsForPost.LEAVE)}
+                        />
+
+                        <img id="share-post" src="/home/share-post.svg" alt="Share Post" width={24} height={24}
+                            onMouseOver={(event) => onButtonsForPostHoverOrLeave(event, OperationsOnButtonsForPost.HOVER)}
+                            onMouseLeave={(event) => onButtonsForPostHoverOrLeave(event, OperationsOnButtonsForPost.LEAVE)} />
+                    </div>
+
+                    <div>
+                        <img id="save" src="/home/save.svg" alt="Save" width={24} height={24}
+                            onMouseOver={(event) => onButtonsForPostHoverOrLeave(event, OperationsOnButtonsForPost.HOVER)}
+                            onMouseLeave={(event) => onButtonsForPostHoverOrLeave(event, OperationsOnButtonsForPost.LEAVE)}
+                        />
                     </div>
                 </div>
+
+                <div className='text-[14px]'>
+                    Liked by <span className='font-medium'>{props.likedBy}</span> and <span className='font-medium'>others</span>
+                </div>
+
+                <div className='text-[13px] text-gray-500 !mt-[5px]'>
+                    View all {props.commentNumber} comments
+                </div>
+
+                <div className='flex justify-between items-center !mt-[5px] text-[14px] space-x-3'>
+                    <TextareaAutosize
+                        value={comment}
+                        className='w-full focus:outline-none resize-none'
+                        placeholder='Add a comment...'
+                        maxRows={4}
+                        onChange={(e) => setComment(e.target.value)}
+                    />
+
+                    {
+                        comment && <div className='text-sky-600 hover:text-black cursor-pointer font-medium'>
+                            Post
+                        </div>
+                    }
+
+                    <div className='flex relative'>
+                        <button className='self-center' onClick={(event) => onEmojiButtonClick(event)}>
+                            <img id="emoji" src="/home/emoji.svg" alt="Emoji" width={24} height={24}
+                                onMouseOver={(event) => onButtonsForPostHoverOrLeave(event, OperationsOnButtonsForPost.HOVER)}
+                                onMouseLeave={(event) => onButtonsForPostHoverOrLeave(event, OperationsOnButtonsForPost.LEAVE)}
+                            />
+                        </button>
+
+                        <div ref={emojiPicker} className='hidden absolute z-10'>
+                            <Picker data={data} onEmojiSelect={onEmojiSelect} />
+                        </div>
+                    </div>
+                </div>
+                <hr />
             </div>
-            <hr />
-        </div>
+            {
+                isMoreMenuOpen && <MoreMenu closeMoreMenu={handleCloseMoreMenu} />
+            }
+        </>
     )
 }
---
+++ src/app/ui/home/more_menu.tsx
@@ -0,0 +1,94 @@
+import { MouseEvent, useEffect, useRef } from "react";
+
+export default function MoreMenu({ closeMoreMenu }: { closeMoreMenu: Function }) {
+    let moreMenu = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        let closeMenuEventListener = (event: Event) => {
+            if (!moreMenu.current?.contains(event.target as Node)) {
+                closeMoreMenu();
+            }
+        };
+
+        document.addEventListener('click', closeMenuEventListener);
+
+        return () => {
+            document.removeEventListener('click', closeMenuEventListener);
+        }
+    })
+
+    function handleMouseDown(event: MouseEvent) {
+        let target = event.target as HTMLDivElement;
+        target.style.backgroundColor = 'rgb(209 213 219)';
+
+        if (target.id === 'report') {
+            target.style.borderRadius = '1rem 1rem 0 0';
+        }
+        else if (target.id === 'cancel') {
+            target.style.borderRadius = '0 0 1rem 1rem';
+        }
+    }
+
+    function handleMouseUp(event: MouseEvent) {
+        let target = event.target as HTMLDivElement;
+        target.style.backgroundColor = 'rgb(255 255 255)';
+    }
+
+    return (
+        <div className="fixed top-0 bottom-0 left-0 right-0 !my-0 w-screen h-screen bg-black/70 z-20">
+            <div className="fixed top-1/2 left-1/2 translate-y-[-50%] translate-x-[-50%]">
+                <div ref={moreMenu} className="more-menu flex flex-col justify-evenly rounded-2xl w-[400px] h-[336px] bg-white opacity-100 text-[14px] divide-y divide-solid">
+                    <div id="report" className="flex-1 flex items-center justify-center cursor-pointer text-red-600 font-semibold"
+                        onMouseDown={(event) => handleMouseDown(event)}
+                        onMouseUp={(event) => handleMouseUp(event)}
+                    >
+                        Report
+                    </div>
+
+                    <div id="go-to-post" className="flex-1 flex items-center justify-center cursor-pointer"
+                        onMouseDown={(event) => handleMouseDown(event)}
+                        onMouseUp={(event) => handleMouseUp(event)}
+                    >
+                        Go to post
+                    </div>
+
+                    <div id="share-to" className="flex-1 flex items-center justify-center cursor-pointer"
+                        onMouseDown={(event) => handleMouseDown(event)}
+                        onMouseUp={(event) => handleMouseUp(event)}
+                    >
+                        Share to...
+                    </div>
+
+                    <div id="copy-link" className="flex-1 flex items-center justify-center cursor-pointer"
+                        onMouseDown={(event) => handleMouseDown(event)}
+                        onMouseUp={(event) => handleMouseUp(event)}
+                    >
+                        Copy link
+                    </div>
+
+                    <div id="embed" className="flex-1 flex items-center justify-center cursor-pointer"
+                        onMouseDown={(event) => handleMouseDown(event)}
+                        onMouseUp={(event) => handleMouseUp(event)}
+                    >
+                        Embed
+                    </div>
+
+                    <div id="about-this-account" className="flex-1 flex items-center justify-center cursor-pointer"
+                        onMouseDown={(event) => handleMouseDown(event)}
+                        onMouseUp={(event) => handleMouseUp(event)}
+                    >
+                        About this account
+                    </div>
+
+                    <div id="cancel" className="flex-1 flex items-center justify-center cursor-pointer"
+                        onMouseDown={(event) => handleMouseDown(event)}
+                        onMouseUp={(event) => handleMouseUp(event)}
+                        onClick={() => closeMoreMenu()}>
+                        Cancel
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
