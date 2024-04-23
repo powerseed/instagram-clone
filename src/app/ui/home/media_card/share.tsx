4800bc69c1a4a63116bd866d88b70c4324b01e2e
Jiehao Luo <powerseed5044@gmail.com>
2024-04-22 23:27:05 -0600

    Make suggested user selection functional

--- src/app/ui/home/media_card/share.tsx
+++ src/app/ui/home/media_card/share.tsx
@@ -1,10 +1,12 @@
-import { useEffect, useRef } from "react";
+import { createRef, useEffect, useRef, useState } from "react";
 import styles from "./styles.module.css";
-import ShareUserCard from "./share_user_card";
+import SuggestedUserCard, { SuggestedUserCardHandle } from "./suggested_user_card";
 import { suggested_users_in_share } from "@/app/content";
 
 export default function Share({ closeShareWindow }: { closeShareWindow: Function }) {
     let shareRef = useRef<HTMLDivElement>(null);
+    let suggestedUserRefs = useRef(suggested_users_in_share.map(() => createRef<SuggestedUserCardHandle>()));
+    let [selectedUsers, setSelectedUsers] = useState<string[]>([]);
 
     useEffect(() => {
         let closeShareEventListener = (event: MouseEvent) => {
@@ -21,6 +23,23 @@ export default function Share({ closeShareWindow }: { closeShareWindow: Function
             document.removeEventListener('click', closeShareEventListener);
         }
     });
+
+    function handleRemoveClick(userToRemove: string) {
+        handleRemoveUser(userToRemove);
+        const userCard = suggestedUserRefs.current?.find(ref => userToRemove === ref.current!.getNickName());
+        userCard!.current!.setCheckedFalse();
+    }
+
+    function handleAddUser(nickname: string) {
+        setSelectedUsers([...selectedUsers, nickname]);
+    }
+
+    function handleRemoveUser(nickname: string) {
+        let copy = [...selectedUsers];
+        let index = copy.indexOf(nickname);
+        copy.splice(index, 1);
+        setSelectedUsers(copy);
+    }
 
     return (
         <div className="fixed top-0 bottom-0 left-0 right-0 w-screen h-screen bg-black/70 z-30">
@@ -40,13 +59,31 @@ export default function Share({ closeShareWindow }: { closeShareWindow: Function
 
                     <hr />
 
-                    <div className="flex flex-0 h-10 items-center space-x-5 px-4">
+                    <div className="flex items-center space-x-5 px-4 mt-[5px] min-h-8">
                         <div className="font-medium">
                             To:
                         </div>
 
-                        <div className="flex-1 text-[13px]">
-                            <input className="outline-none w-full" type="text" placeholder="Search..." />
+                        <div className="flex space-x-1 flex-wrap">
+                            {
+                                selectedUsers.map((selectedUser, index) => {
+                                    return (
+                                        <div key={index} className="mb-[8px] pl-[12px] pr-[6px] py-[2px] flex justify-center items-center space-x-1 rounded-xl bg-sky-100 text-blue-500 text-[14px] font-medium">
+                                            <div>
+                                                {selectedUser}
+                                            </div>
+
+                                            <div className="w-[25px] h-[25px] cursor-pointer" onClick={(e) => handleRemoveClick(selectedUser)}>
+                                                <img src="/home/close-blue.svg" alt="" />
+                                            </div>
+                                        </div>
+                                    )
+                                })
+                            }
+
+                            <div className="flex-1 text-[13px] flex items-center">
+                                <input className="outline-none w-full" type="text" placeholder="Search..." />
+                            </div>
                         </div>
                     </div>
 
@@ -58,16 +95,18 @@ export default function Share({ closeShareWindow }: { closeShareWindow: Function
                         </div>
 
                         {
-                            suggested_users_in_share.map(user => {
+                            suggested_users_in_share.map((user, index) => {
                                 return (
-                                    <div className="px-4 py-[8px] hover:bg-gray-100 cursor-pointer">
-                                        <ShareUserCard
-                                            avatar={user.avatar}
-                                            nickname={user.nickname}
-                                            username={user.username}
-                                            isVerified={user.isVerified}
-                                        />
-                                    </div>
+                                    <SuggestedUserCard
+                                        ref={suggestedUserRefs.current[index]}
+                                        key={user.username}
+                                        avatar={user.avatar}
+                                        nickname={user.nickname}
+                                        username={user.username}
+                                        isVerified={user.isVerified}
+                                        addUser={handleAddUser}
+                                        removeUser={handleRemoveUser}
+                                    />
                                 )
                             })
                         }
