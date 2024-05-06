c929b31fc08278f9da4b75e01cfa2930f4eb3727
Jiehao Luo <powerseed5044@gmail.com>
2024-05-05 23:58:31 -0600

    UI adjustment for Reels

--- src/app/reels/video_card.tsx
+++ src/app/reels/video_card.tsx
@@ -47,67 +47,65 @@ export default function VideoCard(props: VideoCardProps) {
     }
 
     return (
-        <div className="flex w-max space-x-6" >
+        <div className="flex space-x-6" onClick={handleOverlayClick}>
             <div className="relative max-w-[410px] aspect-[0.56] flex items-center bg-black rounded-md">
                 <video ref={videoRef} muted={isMuted} loop>
                     <source src={props.video_src} type="video/mp4" />
                 </video>
 
-                <div className="absolute top-0 left-0 w-full h-full rounded-md flex flex-col justify-between" onClick={handleOverlayClick}>
-                    <div className="flex justify-end mt-3 mr-3">
-                        <div ref={muteButtonRef} className="bg-gray-400/30 hover:bg-gray-400/50 rounded-full px-2 py-2 cursor-pointer" onClick={handleMuteClick}>
-                            <img src={`${isMuted ? '/reels/volume-on.svg' : '/reels/volume-off.svg'}`} alt="Mute" width={16} height={16} />
+                <div className="absolute top-2 right-2 flex justify-end mt-3 mr-3">
+                    <div ref={muteButtonRef} className="bg-gray-400/30 hover:bg-gray-400/50 rounded-full px-2 py-2 cursor-pointer" onClick={handleMuteClick}>
+                        <img src={`${isMuted ? '/reels/volume-off.svg' : '/reels/volume-on.svg'}`} alt="Mute" width={16} height={16} />
+                    </div>
+                </div>
+
+                <div className="absolute left-0 right-0 flex justify-center h-[70px]">
+                    {
+                        !isPlaying &&
+                        <div ref={playButtonRef} className="w-[70px] rounded-full bg-black/50 flex justify-center items-center cursor-pointer" onClick={handlePlayClick}>
+                            <img src='/reels/play.svg' alt="Play" width={24} height={24} />
+                        </div>
+                    }
+                </div>
+
+                <div ref={infoSectionRef} className="absolute w-full bottom-0 flex flex-col space-y-4 px-4 py-4 text-white text-[14px]">
+                    <div className="flex space-x-3 items-center">
+                        <div>
+                            <img className="rounded-full" src={props.avatar} alt="" width={32} height={32} />
+                        </div>
+
+                        <div className="font-[500]">
+                            {props.username}
+                        </div>
+
+                        <div className="px-1 py-1 rounded-md font-[500] border-gray-500 border-[1px]">
+                            Follow
                         </div>
                     </div>
 
-                    <div className="flex justify-center h-[70px]">
-                        {
-                            !isPlaying &&
-                            <div ref={playButtonRef} className="w-[70px] rounded-full bg-black/50 flex justify-center items-center cursor-pointer" onClick={handlePlayClick}>
-                                <img src='/reels/play.svg' alt="Play" width={24} height={24} />
-                            </div>
-                        }
+                    <div className="flex flex-col space-y-4">
+                        <div>
+                            {props.text}
+                        </div>
+
+                        <div className="flex space-x-1 font-[500] flex-wrap">
+                            {props.tags.map((tag) => {
+                                return (
+                                    <span key={tag}>#{tag}</span>
+                                )
+                            })}
+                        </div>
                     </div>
 
-                    <div ref={infoSectionRef} className="flex flex-col space-y-4 px-4 py-4 text-white text-[14px]">
-                        <div className="flex space-x-3 items-center">
-                            <div>
-                                <img className="rounded-full" src={props.avatar} alt="" width={32} height={32} />
-                            </div>
+                    <div className="flex space-x-1">
+                        <img src="/reels/music.svg" alt="" width={13} height={13} />
 
-                            <div className="font-[500]">
-                                {props.username}
-                            </div>
-
-                            <div className="px-1 py-1 rounded-md font-[500] border-gray-500 border-[1px]">
-                                Follow
-                            </div>
+                        <div>
+                            {props.username}
                         </div>
 
-                        <div className="flex flex-col space-y-4">
-                            <div>
-                                {props.text}
-                            </div>
-
-                            <div className="flex space-x-1 font-[500]">
-                                {props.tags.map((tag) => {
-                                    return (
-                                        <span key={tag}>#{tag}</span>
-                                    )
-                                })}
-                            </div>
-                        </div>
-
-                        <div className="flex space-x-1">
-                            <img src="/reels/music.svg" alt="" width={13} height={13} />
-
-                            <div>
-                                {props.username}
-                            </div>
-
-                            <div>
-                                {props.notation}
-                            </div>
+                        <div>
+                            {props.notation}
                         </div>
                     </div>
                 </div>
