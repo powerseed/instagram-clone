7ec8709e8ffe06c308e69b3c3e4cd52a156b3d97
Jiehao Luo <powerseed5044@gmail.com>
2024-05-18 13:40:31 -0600

    Remove "Create" button from Bottom Bar

--- src/app/ui/side_and_bottom_bar/bottom_bar/bottom_bar.tsx
+++ src/app/ui/side_and_bottom_bar/bottom_bar/bottom_bar.tsx
@@ -1,15 +1,6 @@
-import { useState } from "react";
-import BottomBarButton from "./bottom_bar_button";
 import BottomBarLink from "./bottom_bar_link";
-import CreatePostWindow from "../create_post_window/create_post_window";
 
 export default function BottomBar() {
-    let [isCreatePostWindowOpen, setIsCreatePostWindowOpen] = useState(false);
-
-    function onCreateClick() {
-        setIsCreatePostWindowOpen(true);
-    }
-
     return (
         <div className="fixed bottom-0 flex justify-around w-screen h-[var(--bottom-bar-height)] border-t-[1px] md:hidden bg-white">
             <BottomBarLink
@@ -30,13 +21,6 @@ export default function BottomBar() {
                 unselected_icon='/side_and_bottom_bar/reels.svg'
                 selected_icon='/side_and_bottom_bar/reels-selected.svg'
             />
-            <div onClick={onCreateClick}>
-                <BottomBarButton
-                    text='Create'
-                    unselected_icon='/side_and_bottom_bar/create.svg'
-                    selected_icon='/side_and_bottom_bar/create.svg'
-                />
-            </div>
             <BottomBarLink
                 href='/direct/inbox'
                 text='Messages'
@@ -49,9 +33,6 @@ export default function BottomBar() {
                 unselected_icon='/profile.png'
                 selected_icon={undefined}
             />
-            {
-                isCreatePostWindowOpen && <CreatePostWindow closeThisWindow={() => setIsCreatePostWindowOpen(false)} />
-            }
         </div>
     )
 }
