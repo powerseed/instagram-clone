ea7312f0c18dfdbbacc1fdd59516110828aa9fe8
Jiehao Luo <powerseed5044@gmail.com>
2024-05-22 18:47:56 -0600

    Add textarea to Create Post Window

--- src/app/ui/side_and_bottom_bar/create_post_window/add_info.tsx
+++ src/app/ui/side_and_bottom_bar/create_post_window/add_info.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 type AddInfoProps = {
     mediaUrl: string,
     goPreviousStep: () => void,
@@ -5,8 +7,16 @@ type AddInfoProps = {
 }
 
 export default function AddInfo(props: AddInfoProps) {
+    const charLimit = 2200;
+    let [charCount, setCharCount] = useState<number>(0);
+
     function handleShareClick() {
         props.closeThisWindow();
+    }
+
+    function handleTextareaInput(event: React.FormEvent<HTMLTextAreaElement>) {
+        const target = event.currentTarget!;
+        setCharCount(target.value.length);
     }
 
     return (
@@ -27,20 +37,28 @@ export default function AddInfo(props: AddInfoProps) {
 
             <div className="grow flex">
                 <div className="flex justify-center items-center w-[calc(100%-340px)] border-r-[1px]">
-                    <img className="h-full" src={props.mediaUrl} alt="Cropped Image" />
+                    <img className="h-full rounded-bl-2xl" src={props.mediaUrl} alt="Cropped Image" />
                 </div>
 
-                <div className="w-[340px]">
-                    <div>
+                <div className="w-[340px] flex flex-col">
+                    <div className="flex px-4 py-3 items-center space-x-2">
+                        <img src="/profile.png" alt="" width={36} height={36} />
 
+                        <div className="font-[500]">
+                            walterwhite
+                        </div>
                     </div>
 
-                    <div>
-
+                    <div className="grow px-4">
+                        <textarea className="w-full h-full resize-none outline-none placeholder:text-gray-300 placeholder:text-[15px]"
+                            placeholder="Write a caption..."
+                            maxLength={charLimit}
+                            onInput={handleTextareaInput}
+                        />
                     </div>
 
-                    <div>
-                        
+                    <div className="text-[12px] text-gray-400 flex justify-end px-4 py-2">
+                        {charCount}/{charLimit}
                     </div>
                 </div>
             </div>
