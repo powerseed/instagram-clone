399c7574767c6f3d2ae5fe144471f3c936b45e99
Jiehao Luo <powerseed5044@gmail.com>
2024-05-23 21:14:56 -0600

    Basic UI for Big Screen of Profile page

--- src/app/[username]/page.tsx
+++ src/app/[username]/page.tsx
@@ -19,79 +19,100 @@ export default function Profile() {
     }
 
     return (
-        <div className="w-full h-[calc(100vh-var(--bottom-bar-height))] flex flex-col">
-            <div className="flex px-5 py-5 justify-between items-center">
-                <img src="/profile.jpg" alt="avatar" width={77} height={77} />
+        <div className="w-full max-w-[935px] h-[calc(100vh-var(--bottom-bar-height))] flex flex-col">
+            <div className="grid grid-cols-[120px_1fr] sm:grid-cols-[1fr_2fr] grid-rows-[auto_auto_auto] sm:grid-rows-[auto_auto_auto_auto_auto] mt-[20px]">
+                <div className="col-start-1	row-start-1 row-end-2 sm:row-end-4 ml-[16px] sm:ml-0 mr-[28px] flex justify-center">
+                    <img className="sm:w-[150px] sm:h-[150px]" src="/profile.jpg" alt="avatar" />
+                </div>
 
-                <div className="h-full flex flex-col justify-between">
-                    <div className="text-[20px] py-2">
+                <div className="col-start-2	row-start-1 row-end-2 flex flex-wrap items-center sm:mb-[20px]">
+                    <div className="mr-5 text-[20px] py-2">
                         walterwhite
                     </div>
 
                     <div className="flex text-[13px] space-x-2">
-                        <div className="font-[500] bg-gray-300 px-5 py-1 rounded-md">
+                        <div className="font-[500] bg-gray-300 px-5 py-1 h-fit rounded-md">
                             Edit profile
                         </div>
 
-                        <div className="font-[500] bg-gray-300 px-5 py-1 rounded-md">
+                        <div className="font-[500] bg-gray-300 px-5 py-1 h-fit rounded-md">
                             View archive
+                        </div>
+
+                        <div className="hidden sm:block cursor-pointer">
+                            <img src="/profile/config.svg" alt="Configuration" width={24} height={24} />
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-start-1	sm:col-start-2 col-end-3 row-start-2 sm:row-start-3 row-end-3 sm:row-end-4 px-5 sm:px-0 py-4 sm:py-0 font-[500] text-[14px] border-b-[1px] sm:border-none flex items-center">
+                    WalterWhite
+                </div>
+
+                <div className="col-start-1 sm:col-start-2 col-end-3 row-start-3 sm:row-start-2 row-end-4 sm:row-end-3 py-2 sm:py-0 sm:mb-[20px] flex justify-evenly sm:justify-start sm:space-x-8 items-center text-[14px] border-b-[1px] sm:border-none font-[500]">
+                    <div className="flex flex-col sm:flex-row sm:space-x-1 items-center">
+                        <div>
+                            {postCount}
+                        </div>
+
+                        <div className="text-gray-400 sm:text-black">
+                            posts
+                        </div>
+                    </div>
+
+                    <div className="flex flex-col sm:flex-row sm:space-x-1 items-center">
+                        <div>
+                            {followerCount}
+                        </div>
+
+                        <div className="text-gray-400 sm:text-black">
+                            followers
+                        </div>
+                    </div>
+
+                    <div className="flex flex-col sm:flex-row sm:space-x-1 items-center">
+                        <div>
+                            {followingCount}
+                        </div>
+
+                        <div className="text-gray-400 sm:text-black">
+                            following
                         </div>
                     </div>
                 </div>
             </div>
 
-            <div className="px-5 pb-4 font-[500] text-[14px] border-b-[1px]">
-                WalterWhite
-            </div>
-
-            <div className="py-3 flex justify-evenly text-[14px] border-b-[1px]">
-                <div className="flex flex-col items-center">
-                    <div>
-                        {postCount}
+            <div className="hidden sm:flex mx-8">
+                <div className="my-12 flex flex-col justify-center items-center space-y-3 cursor-pointer">
+                    <div className="flex justify-center items-center rounded-full w-[85px] h-[85px] border-[1px]">
+                        <img src="/profile/plus.svg" alt="" width={48} height={48} />
                     </div>
 
-                    <div className="text-gray-400">
-                        posts
-                    </div>
-                </div>
-
-                <div className="flex flex-col items-center">
-                    <div>
-                        {followerCount}
-                    </div>
-
-                    <div className="text-gray-400">
-                        followers
-                    </div>
-                </div>
-
-                <div className="flex flex-col items-center">
-                    <div>
-                        {followingCount}
-                    </div>
-
-                    <div className="text-gray-400">
-                        following
+                    <div className="text-[12px] font-[500]">
+                        New
                     </div>
                 </div>
             </div>
 
             <div className="py-3 flex justify-evenly border-b-[1px]">
-                <img src={tabSelected === Tabs.POSTS ? `/profile/posts-selected.svg` : `/profile/posts-unselected.svg`}
+                <img className="cursor-pointer"
+                    src={tabSelected === Tabs.POSTS ? `/profile/posts-selected.svg` : `/profile/posts-unselected.svg`}
                     alt="Posts"
                     width={24}
                     height={24}
                     onClick={() => handleTabClick(Tabs.POSTS)}
                 />
 
-                <img src={tabSelected === Tabs.SAVE ? `/profile/save-selected.svg` : `/profile/save-unselected.svg`}
+                <img className="cursor-pointer"
+                    src={tabSelected === Tabs.SAVE ? `/profile/save-selected.svg` : `/profile/save-unselected.svg`}
                     alt="Saves"
                     width={24}
                     height={24}
                     onClick={() => handleTabClick(Tabs.SAVE)}
                 />
 
-                <img src={tabSelected === Tabs.PHOTO ? `/profile/photo-selected.svg` : `/profile/photo-unselected.svg`}
+                <img className="cursor-pointer"
+                    src={tabSelected === Tabs.PHOTO ? `/profile/photo-selected.svg` : `/profile/photo-unselected.svg`}
                     alt="Photos"
                     width={24}
                     height={24}
