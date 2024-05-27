e92112ad276cbfe0f23151922d6f4d2d60c09c07
Jiehao Luo <powerseed5044@gmail.com>
2024-05-27 13:00:34 -0600

    Make authOptions a separate file

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,31 +1,5 @@
 import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
-import GoogleProvider from "next-auth/providers/google"
-import TwitterProvider from "next-auth/providers/twitter"
-
-export const authOptions = {
-    providers: [
-        GithubProvider(
-            {
-                clientId: String(process.env.GITHUB_ID),
-                clientSecret: String(process.env.GITHUB_SECRET),
-            }
-        ),
-        GoogleProvider(
-            {
-                clientId: String(process.env.GOOGLE_CLIENT_ID),
-                clientSecret: String(process.env.GOOGLE_CLIENT_SECRET),
-            }
-        ),
-        TwitterProvider(
-            {
-                clientId: String(process.env.TWITTER_CLIENT_ID),
-                clientSecret: String(process.env.TWITTER_CLIENT_SECRET),
-                version: "2.0",
-            }
-        ),
-    ],
-}
+import { authOptions } from './auth_options';
 
 const handler = NextAuth(authOptions);
 
